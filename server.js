decc58cc3bcfe8baedae3dd1d9b30b922ce1132f
Clarefollett <chlaw101@gmail.com>
2017-03-24 11:52:44 -0700

    added basic server file for intial connection

---
+++ server.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+mongoose.Promise = Promise;
+
+const dbUri = process.env.DB_URI || 'mongodb://localhost:27017/colordiary';
+
+module.exports = mongoose.connection(dbUri);
+
+mongoose.connection.on('connection', () => {
+    console.log('connection was successful')
+});
