9aeab226fba001e2f1515c9162b8e034841f1030
Lynn <code@krogseng.com>
2017-03-26 01:44:30 -0700

    Auth allows signup & signin.

---
+++ server.js
@@ -0,0 +1,12 @@
+const app = require('./lib/app');
+const http = require('http');
+const PORT = process.env.PORT || 3001;
+require('./lib/connection');
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+    console.log('server running on ', server.address());
+});
+
+
