1935892bcc9f17ccfe773682b59dcc895bcfed63
Clarefollett <chlaw101@gmail.com>
2017-03-28 13:18:50 -0700

    got the weather object populating in the test, I need to restructure the test so it knows what request/response is w/o doing req, res, next

--- test/mood-schema.test.js
+++ test/mood-schema.test.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
 const Schema = mongoose.Schema;
 const assert = require('chai').assert;
 const User = require('../lib/models/user-schema');
@@ -7,19 +6,26 @@ const Mood = require('../lib/models/mood-schema');
 const Color = require('../lib/models/color-schema');
 const Block = require('../lib/models/block-schema');
 
+
 describe('saves new user, color, and block number so we can test and populate the mood schema', () => {
     
-    it('tests the mood schema with each field populated', () => {
-        //saving new user
-        const userOne = new User({ 
-            username: 'userOne',
-            email: 'colordiary@gmail.com',
-            password: 'password',
+    it('fails if missing required field', (done) => {
+
+        failMood = new Mood({
+            date: new Date,
+            comment: 'it was a rainy monday',
+            weather: {description: 'partly cloudy'},
+            zipcode: 97239        
         })
-        userOne.save(function(err) {
-            if (err) return res.end();
-            return res.sendStatus(201);
-        });
+        //expecting test to fail w/o required fields
+        //validate function throws error which means passing test
+        failMood.validate(err => {
+            if (err) done();
+            else done('fail');
+        })
+    })
+
+    it('tests the mood schema with each field populated', (done) => {
         //saving new color
         const colorOne = new Color({
             index: 0,
@@ -27,29 +33,29 @@ describe('saves new user, color, and block number so we can test and populate th
             hexColor: '#8181FC',
             mood: 'sad',
         })
-        colorOne.save(function(err) {
-            if (err) return res.end();
-            return res.sendStatus(201);
-        });
         //saving new block
         const blockOne = new Block({
             blockNumber: 0,
             timeFrame: 'morning'
         })
-        blockOne.save(function(err) {
-            if (err) return res.end();
-            return res.sendStatus(201);
-        });
         //populating the mood schema and testing
-        const moodOne = new Mood({
-            userId: userOne._id,
+        correctMood = new Mood({
+            userId: new mongoose.Types.ObjectId,
             date: new Date,
-            color: colorOne._id,
+            color: new mongoose.Types.ObjectId,
             comment: 'it was a rainy monday',
-            block: blockOne._id,
-            weather: {},
+            block: new mongoose.Types.ObjectId,
+            weather: {description: 'partly cloudy'},
             zipcode: 97239        
         })
-        console.log(moodOne)
+        assert.isDefined(correctMood.userId)
+        assert.isDefined(correctMood.date)
+        assert.isDefined(correctMood.color)
+        assert.isDefined(correctMood.block)
+        
+        correctMood.validate(err => {
+            if (!err) done();
+            else done(err);
+        });
     })
 });
--- test/weather-api.test.js
+++ test/weather-api.test.js
@@ -1,42 +1,49 @@
-const chai = require('chai');
-const chaiHttp = require('chai-Http');
 const mongoose = require('mongoose');
-
-const app = require('../lib/app');
-
-const assert = chai.assert;
-
+const chai = require('chai');
+const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
+const assert = chai.assert;
+const app = require('../lib/app');
+const request = chai.request(app);
+const token = require('../lib/auth/token');
+const User = require('../lib/models/user-schema');
+const Mood = require('../lib/models/mood-schema');
+const Color = require('../lib/models/color-schema');
+const Block = require('../lib/models/block-schema');
+const weatherApi = require('../lib/routes/weather-api');
+const apiKey = process.env.WEATHER_API_KEY;
+const requestProxy = require('request-promise');
 
-process.env.DB_URL = 'mongodb://localhost:27017/colordiary-test';
-require('../lib/connection');
+const newEntry = new Mood({
+    userId: new mongoose.Types.ObjectId,
+    date: new Date,
+    color: new mongoose.Types.ObjectId,
+    comment: 'it was a rainy monday',
+    block: new mongoose.Types.ObjectId,
+    weather: {},
+    zipcode: 97239
+})
 
-describe('weather api route', () => {
+describe('outside api for weather', () => {
 
-    const request = chai.request(app);
-
-    const moodOne = new Mood({
-        userId: 12345,
-        date: new Date,
-        color: 3415,
-        comment: 'it was a rainy monday',
-        block: 332,
-        weather: {},
-        zipcode: 97239        
-    })
-
-    before(() => mongoose.connection.dropDatabase());
-
-    it('posts a new entry', () => {
-        return request.post('/contacts')
-            .send(contactOne)
+    it('fuck', (res, req, next) => {
+        requestProxy(`http://api.wunderground.com/api/${apiKey}/conditions/q/${newEntry.zipcode}.json`)
+            .then(
+                result => {
+                    result = JSON.parse(result)
+                    newEntry.weather = {
+                        temp: result.current_observation.temperature_string,
+                        feelsLike: result.current_observation.feelslike_string,
+                        description: result.current_observation.weather,
+                        location: result.current_observation.observation_location.full,
+                        country: result.current_observation.observation_location.country
+                    }
+                }
+            )
             .then(res => {
-                contactOne = res.body;
-                assert.equal(res.body.name, 'Claire Follett')
-                assert.equal(res.body.email, 'chlaw101@gmail.com')
-                assert.equal(res.body.company, 'Code Fellows')
-                assert.equal(res.body.notes, 'loves dogs')
-                assert.equal(res.body.category, 'school')
-                assert.equal(res.body._id, contactOne._id)
+                res = newEntry;
+                console.log(res)
             })
+        })
     })
+
